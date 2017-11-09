89777ede37e4f968c3a3e1cd4d93999d89e7e1c9
DmytroK <lstswtd@gmail.com>
2017-11-09 13:19:35 +0100

    modified_to_circ_and_area

--- test.py
+++ test.py
@@ -7,3 +7,11 @@ def circumference (myradius):
     return circum
 
 #this is a function to calculate the circumference
+
+import math
+# This function calculates the are of the circle
+# it takes the radius as an input
+# Also relies on the math package
+def areacirc (myradius):
+    myarea = math.pi * myradius ** 2
+    return myarea
